476ab8786e7b784f71c9825e4d5423cd5778fd79
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-07-18 17:08:07 +0530

    49 - API - Creating a Buggy Controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+            if(thing == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerRequest()
+        {
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString(); // Null reference excep
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestById(int id)
+        {
+            return Ok();
+        }   
+    }
+}
